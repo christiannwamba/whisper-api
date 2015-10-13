3f52f1f7349159308045cf639b0ed667dea52972
christiannwamba <nwambachristian@gmail.com>
2015-10-13 07:07:34 -0700

    launch

--- config/connections.js
+++ config/connections.js
@@ -52,7 +52,7 @@ module.exports.connections = {
   //Used in sails.config.env.production
   remoteMysqlServer: {
     adapter: 'sails-mysql',
-    host: 'us-cdbr-iron-east-03.cleardb.net?reconnect=true',
+    host: 'us-cdbr-iron-east-03.cleardb.net',
     user: 'b813f4b29c6f95',
     password: '679e67fd',
     database: 'heroku_3b4a284406d4f11'
